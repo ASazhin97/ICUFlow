66a38b1cd3c9235561beefc6b6861792c0b795a9
KyleHorton <kyle.horton@quinnipiac.edu>
2018-10-29 22:40:40 -0400

    updates

---
+++ public/js/inputDigest.js
@@ -0,0 +1,6 @@
+function saveData(){
+  var inputData = document.getElementById('inputNurse').value;
+  var result = document.getElementByID('printData');
+
+  return result.textContext = 'Name: ' + inputData;
+}
